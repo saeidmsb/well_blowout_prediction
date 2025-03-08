c274a1f037693baf057d768164cdadb834a48a1e
saeed <basserims@gmail.com>
2025-03-08 17:20:16 +0330

    commit

--- well_blowout_prediction/src/__init__.py
+++ well_blowout_prediction/src/__init__.py
@@ -1,5 +1,4 @@
-# فایل __init__.py برای تعیین این دایرکتوری به عنوان یک پکیج پایتونی استفاده می‌شود.
-# می‌توان در اینجا تنظیمات اولیه یا واردات اصلی را قرار داد.
+
 
 from .data_loader import load_sensor_data, create_sequences
 from .training import train_model
